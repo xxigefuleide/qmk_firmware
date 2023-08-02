c362f247e50e21934ae8d056c5bce58eae0b243c
weimao <luanty@163.com>
2023-08-02 15:40:58 +0800

    add planck_blueism

---
+++ keyboards/planck_blueism/keymaps/default/keymap.c
@@ -0,0 +1,45 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "blueism.h"
+
+enum keycodes{
+    UNPAIR = SAFE_RANGE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │Tab│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │Bsp│
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Esc│ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │Sft│ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Ent│
+     * ├───┼───┼───┼───┼───┼───┴───┼───┼───┼───┼───┼───┤
+     * │App│Ctl│Alt│GUI│ < │       │ > │ ← │ ↓ │ ↑ │ → │
+     * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_planck_mit(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_APP,  KC_LCTL, KC_LALT, KC_LGUI, UNPAIR,       KC_SPC,       KC_GT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case UNPAIR:
+      if (record->event.pressed) {
+        // Do something when pressed
+        blueism_unpair();
+      } else {
+        // Do something else when release
+      }
+      return false;
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
---
+++ keyboards/planck_blueism/rules.mk
@@ -0,0 +1,4 @@
+BLUETOOTH_ENABLE = yes
+BLUETOOTH_DRIVER = custom
+
+include keyboards/planck_blueism/blueism/blueism.mk
